3e7c38777aff8131e980241b8e3df52653e22a28
pde-bakk <pde-bakk@student.codam.nl>
2021-10-13 19:59:55 +0200

    assert true

--- srcs/parsing/parsing_file.py
+++ srcs/parsing/parsing_file.py
@@ -18,4 +18,6 @@ def parserow(row: str) -> list[int]:
 
 def assert_validity(size: int, rows: np.ndarray) -> None:
 	assert size == rows.shape[0] == rows.shape[1]
-	assert size * size == len(set(rows.flatten()))
+	puzzle_as_set = set(rows.flatten())
+	assert len(puzzle_as_set) == size * size
+	assert all(x in range(0, size * size) for x in puzzle_as_set)
