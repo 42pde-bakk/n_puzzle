7eb66e9feb77f01b630a89854463d437c2adab20
pde-bakk <pde-bakk@student.codam.nl>
2021-10-18 19:46:04 +0200

    moved validation rules to puzzle_validator.py. As a bonus I figured out why my Direction.__str__() wasn't working: I wasn't calling it...

--- srcs/main.py
+++ srcs/main.py
@@ -3,13 +3,14 @@ import sys
 from srcs.npuzzle import Npuzzle
 from srcs.astar import Astar
 from srcs.heuristics import manhattan_distance
+from srcs.puzzle_validator import PuzzleValidator
 
 
 def main(npuzzle_file):
 	with open(npuzzle_file, 'r') as f:
 		puzzle = Npuzzle()
 		puzzle.parse_puzzle(f.read().splitlines())
-	if puzzle.is_solvable():
+	if PuzzleValidator.is_valid(puzzle) and PuzzleValidator.is_solvable(puzzle):
 		astar = Astar(puzzle, manhattan_distance)
 		astar.solve()
 	else:
