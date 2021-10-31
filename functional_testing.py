1df6fa4a447818292f11b56856a5d657023c88b3
pde-bakk <pde-bakk@student.codam.nl>
2021-10-31 21:48:49 +0100

    uncommented unsolvables

--- functional_testing.py
+++ functional_testing.py
@@ -4,11 +4,11 @@ import sys
 
 
 class MyTestCase(unittest.TestCase):
-	# def test_unsolvable(self):
-	# 	for filename in ["npuzzle-3-unsolvable.txt", "npuzzle-3-unsolvable2.txt", "npuzzle-4-unsolvable.txt"]:
-	# 		# Idk why the exit code turns into 256 when I exit with 1 but OK
-	# 		print(f'unsolvabe {filename}')
-	# 		self.assertEqual(256, os.system(f'python3 srcs/main.py puzzles/{filename}'))
+	def test_unsolvable(self):
+		for filename in ["npuzzle-3-unsolvable.txt", "npuzzle-3-unsolvable2.txt", "npuzzle-4-unsolvable.txt"]:
+			# Idk why the exit code turns into 256 when I exit with 1 but OK
+			print(f'unsolvabe {filename}')
+			self.assertEqual(256, os.system(f'python3 srcs/main.py puzzles/{filename}'))
 
 	def test_solvable(self):
 		for filename in ["npuzzle-3-1.txt", "npuzzle-3-2.txt", "npuzzle-4-1.txt"]:
