2baf9820147f47b9ec27c9da9a8275652a1ca60f
pde-bakk <pde-bakk@student.codam.nl>
2021-10-31 16:48:07 +0100

    linted main.py

--- srcs/main.py
+++ srcs/main.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 import sys
-from srcs.gamestate import Gamestate
 from srcs.puzzle import Puzzle
 from srcs.astar import Astar
-from srcs.heuristics import mannhattan_distance, misplaced_tiles
 from srcs.validator import PuzzleValidator
 
 
 def main(npuzzle_file):
+	"""Open the given argument, parse it, validate it and start the search"""
 	with open(npuzzle_file, 'r') as f:
 		puzzle = Puzzle()
 		puzzle.parse_puzzle(f.read().splitlines())
 
 	if PuzzleValidator.is_valid(puzzle) and PuzzleValidator.is_solvable(puzzle):
-		astar = Astar(puzzle, puzzle.create_starting_state(), mannhattan_distance)
+		astar = Astar(puzzle, puzzle.create_starting_state())
 		astar.solve()
 		if astar.solution is not None:
 			astar.statistics.show_statistics(astar.solution)
 		else:
-			print(f'I failed at solving the puzzle')
+			print('I failed at solving the puzzle')
 	else:
-		print(f'Puzzle is not solvable')
+		print('Puzzle is not solvable')
 
 
 if __name__ == "__main__":
