21722c9641b992b8cd128cf1a1e17ff5bed2c7bf
pde-bakk <pde-bakk@student.codam.nl>
2021-11-14 18:20:47 +0100

    Added extra parsing checks

--- srcs/main.py
+++ srcs/main.py
@@ -15,37 +15,52 @@ def parse_arguments():
 	# action='store_true' to not require a new value after the argument
 	parser.add_argument('filepath', help='Filepath for the puzzle file.')
 	parser.add_argument('--cprofile', action='store_true', help='Run cProfile to see where most time is spent.')
-	parser.add_argument('--verbose', '-v', action='store_true', help='Print verbose information about each step of the Astar algorithm.')
+	parser.add_argument('--verbose', '-v', action='store_true',
+	                    help='Print verbose information about each step of the Astar algorithm.')
 	parser.add_argument('--uniform', '-u', action='store_true',
-		help='Find the shortest path with just the movecost, no heuristic!')
+	                    help='Find the shortest path with just the movecost, no heuristic!')
 	parser.add_argument('--greedy', '-g', action='store_true', help='Run a greedy search. \
 		This does not take the amount of moves into account and therefore might not produce the optimal path, \
 		but will find a solution quicker!', default=False)
 	# Heuristics
 	heuristics_group = parser.add_argument_group('Heuristics', description='Argument group for heuristics')
-	heuristics_group.add_argument('--manhattan', action='store_true', default=False, help='Use the Manhattan distance heuristic')
-	heuristics_group.add_argument('--weightedmanhattan', action='store_true', default=False, help='Use the Manhattan distance heuristic but give extra priority to edge and especially corner pieces')
-	heuristics_group.add_argument('--misplaced', action='store_true', default=False, help='Use the amount of misplaced tiles as heuristic')
-	heuristics_group.add_argument('--euclidean', action='store_true', default=False, help='Use the Euclidean distance heuristic')
-	heuristics_group.add_argument('--minkowski', action='store_true', default=False, help='Use the Minkowski distance heuristic')
-	heuristics_group.add_argument('--incorrectlines', action='store_true', default=False, help='Count how many rows and columns are fully correct')
+	heuristics_group.add_argument('--manhattan', action='store_true', default=False,
+	                              help='Use the Manhattan distance heuristic')
+	heuristics_group.add_argument('--weightedmanhattan', action='store_true', default=False,
+	                              help='Use the Manhattan distance heuristic but give extra priority to edge and especially corner pieces')
+	heuristics_group.add_argument('--misplaced', action='store_true', default=False,
+	                              help='Use the amount of misplaced tiles as heuristic')
+	heuristics_group.add_argument('--euclidean', action='store_true', default=False,
+	                              help='Use the Euclidean distance heuristic')
+	heuristics_group.add_argument('--minkowski', action='store_true', default=False,
+	                              help='Use the Minkowski distance heuristic')
+	heuristics_group.add_argument('--incorrectlines', action='store_true', default=False,
+	                              help='Count how many rows and columns are fully correct')
 
 	args = parser.parse_args()
-	if not (args.manhattan or args.weightedmanhattan or args.misplaced or args.minkowski or args.euclidean or args.incorrectlines):
+	if not (args.manhattan or args.weightedmanhattan or args.misplaced or
+			args.minkowski or args.euclidean or args.incorrectlines):
 		args.weightedmanhattan = True
+	if args.uniform and args.greedy:
+		print('You can\'t run both a uniform and a greedy search at the same time, dummy!\n'
+				'That\'d just be a uniform search', file=sys.stderr)
 	return args
 
 
 def main(args) -> int:
 	"""Open the given argument, parse it, validate it and start the search"""
 	pr = cProfile.Profile()
-	with open(args.filepath, 'r') as f:
-		puzzle = Puzzle()
-		try:
-			puzzle.parse_puzzle(f.read().splitlines())
-		except ValueError:
-			print('Puzzle is invalid', file=sys.stderr)
-			return 1
+	try:
+		with open(args.filepath, 'r') as f:
+			puzzle = Puzzle()
+			try:
+				puzzle.parse_puzzle(f.read().splitlines())
+			except ValueError:
+				print('Puzzle is invalid', file=sys.stderr)
+				return 1
+	except FileNotFoundError:
+		print(f'{args.filepath} does not exist, please provide a valid filepath to the puzzle.', file=sys.stderr)
+		return 1
 
 	if not PuzzleValidator.is_valid(puzzle):
 		return 1
@@ -71,9 +86,5 @@ def main(args) -> int:
 
 
 if __name__ == "__main__":
-	arguments = parse_arguments()
-	# if arguments.cprofile:
-	# 	exit_code = cProfile.runctx('f(x)', {'f': main, 'x': arguments}, {})
-	# else:
 	EXIT_CODE = main(parse_arguments())
 	sys.exit(EXIT_CODE)
