6c5d1d552c7922c2dc7338f4c099d4b05741bd16
pde-bakk <pde-bakk@student.codam.nl>
2021-11-26 14:51:15 +0100

    added beamsearch option

--- srcs/main.py
+++ srcs/main.py
@@ -5,6 +5,7 @@ import pstats
 from argparse import ArgumentParser
 from puzzle import Puzzle
 from astar import Astar
+from beamsearch import Beamsearch
 from validator import PuzzleValidator
 
 
@@ -22,6 +23,9 @@ def parse_arguments():
 	parser.add_argument('--greedy', '-g', action='store_true', help='Run a greedy search. \
 		This does not take the amount of moves into account and therefore might not produce the optimal path, \
 		but will find a solution quicker!', default=False)
+
+	parser.add_argument('--algo', action='store', default='astar')
+
 	# Heuristics
 	heuristics_group = parser.add_argument_group('Heuristics', description='Argument group for heuristics')
 	heuristics_group.add_argument('--manhattan', action='store_true', default=False,
@@ -40,7 +44,10 @@ def parse_arguments():
 	args = parser.parse_args()
 	if not (args.manhattan or args.weightedmanhattan or args.misplaced or
 			args.minkowski or args.euclidean or args.incorrectlines):
-		args.weightedmanhattan = True
+		args.manhattan = True
+	print(f'algo is {args.algo}')
+	if args.algo != 'beamsearch' and args.algo != 'astar':
+		args.algo = 'astar'
 	if args.uniform and args.greedy:
 		print('You can\'t run both a uniform and a greedy search at the same time, dummy!\n'
 				'That\'d just be a uniform search', file=sys.stderr)
@@ -66,18 +73,21 @@ def main(args) -> int:
 		return 1
 
 	if PuzzleValidator.is_solvable(puzzle):
-		astar = Astar(puzzle, puzzle.create_starting_state(), args)
+		if args.algo == 'beamsearch':
+			search = Beamsearch(puzzle, puzzle.create_starting_state(), args)
+		else:
+			search = Astar(puzzle, puzzle.create_starting_state(), args)
 		if args.cprofile:
 			pr.enable()
-		astar.solve()
+		search.solve()
 		if args.cprofile:
 			pr.disable()
 			stats = pstats.Stats(pr)
 			stats.sort_stats('tottime').print_stats(10)
 			return 0
 
-		if astar.solution is not None:
-			astar.statistics.show_statistics(astar.solution)
+		if search.solution is not None:
+			search.statistics.show_statistics(search.solution)
 			return 0
 		print('I failed at solving the puzzle', file=sys.stderr)
 	else:
---
+++ srcs/search.py
@@ -0,0 +1,85 @@
+import copy
+import heapq
+import numpy as np
+from heuristics import set_heuristic_values, set_heuristic_values_timeoptimized
+from gamestate import Gamestate, Direction
+from statistics_class import Statistics
+from puzzle import Puzzle
+tiebreaker = 0
+
+
+def push_to_heap(queue: [], node: Gamestate) -> None:
+	"""Wrapper function to push to the heapq and increment the tiebreaker value"""
+	global tiebreaker
+	heapq.heappush(queue, (node.g + node.h_total, node.h_total, tiebreaker, node))
+	tiebreaker += 1
+
+
+class Search:
+	"""Astar algorithm class"""
+	def __init__(self, puzzle: Puzzle, original: Gamestate, args):
+		self.solution = None
+		self.args = args
+		self.open_queue = []
+		self.closed_queue = {}
+		self.puzzle = puzzle
+		self.statistics = Statistics()
+		self.queue_first_node(original)
+
+	def queue_first_node(self, node: Gamestate) -> None:
+		"""Set heuristic values for the original gamestate and push to heap"""
+		node_as_bytes = node.rows.tobytes()
+		if not self.args.greedy:
+			node.g = node.moves
+		set_heuristic_values(node, self.puzzle.goal_matrix, self.args)
+		seen = bool(node_as_bytes in self.closed_queue)
+
+		if not seen or node.moves < self.closed_queue[node_as_bytes]:
+			push_to_heap(self.open_queue, node=node)
+
+	def queue_node(self, node: Gamestate) -> None:
+		"""Method to push value to queue if there wasn't already a better gamestate like this in the queue"""
+		node_as_bytes = node.rows.tobytes()
+		if not self.args.greedy:
+			node.g = node.moves
+		set_heuristic_values_timeoptimized(node, self.puzzle.goal_matrix, self.args)
+		seen = bool(node_as_bytes in self.closed_queue)
+
+		if not seen or node.moves < self.closed_queue[node_as_bytes]:
+			push_to_heap(self.open_queue, node=node)
+
+	def add_node_to_closed_queue(self, node: Gamestate) -> None:
+		"""Add gamestate node to the closed queue, and update it's value if it already existed"""
+		node_as_bytes = node.rows.tobytes()
+
+		if node_as_bytes not in self.closed_queue or self.closed_queue[node_as_bytes] > node.g:
+			self.closed_queue[node_as_bytes] = node.g
+
+	def spawn_successors(self, state: Gamestate):
+		"""Spawn children of the most promising gamestate"""
+		if self.args.verbose:
+			print(f'Expanding node with h_total: {state.h_total}\n{state}\n')
+		self.add_node_to_closed_queue(state)
+		for direction in Direction:
+			try:
+				state.is_possible(direction)
+				successor = copy.deepcopy(state)
+				successor.do_move(direction)
+				self.queue_node(node=successor)
+			except (AssertionError, KeyError):
+				pass
+		self.statistics.track_size_complexity(len(self.open_queue) + len(self.closed_queue))
+
+	def explore_node(self, node: tuple):
+		heuristic_value, _, _, state = node
+		self.statistics.increment_time_complexity()
+		try:
+			as_bytes = state.rows.tobytes()
+			if heuristic_value >= self.closed_queue[as_bytes]:
+				return
+		except KeyError:
+			pass
+		if np.array_equal(state.rows, self.puzzle.goal_matrix):
+			self.solution = state
+			return
+		self.spawn_successors(state)
